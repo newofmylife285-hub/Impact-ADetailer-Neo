d512d174afe392b470bfeeef542a5b361a7a52e5
sca285 <newofmylife285@gmail.com>
2026-09-17 21:42:33 +0700

    Delete preload.py

--- preload.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-
-def preload(parser: argparse.ArgumentParser):
-    parser.add_argument(
-        "--ad-no-huggingface",
-        action="store_true",
-        help="Do not automatically download YOLO models",
-    )
